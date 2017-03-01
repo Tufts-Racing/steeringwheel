1dbfb17a07fbccb031b073dd2c564d010cf71185
petermalinovsky <peter@malinovsky.net>
2017-02-28 21:19:26 -0500

    v1.0

---
+++ wheel_code/pins.h
@@ -0,0 +1,33 @@
+#ifndef PINS_H
+#define PINS_H
+
+
+// Display Pins
+#define OLED_CLK 13
+#define OLED_MOSI 11
+#define OLED_CS 10
+#define OLED_RESET 9
+#define OLED_DC 8
+
+// Buttons/Switches
+#define SW1_L_IN 2
+#define SW1_R_IN 3
+#define TOGGLE_L_IN A0
+#define TOGGLE_R_IN A1
+
+//LEDs
+#define LED_R A3
+
+//Shift Registers
+#define SERIAL_1 7
+#define CLK_1 5
+#define CLR_1 1
+
+#define SERIAL_2 6
+#define CLK_2 4
+#define CLR_2 0
+
+#define left 0
+#define right 1
+
+#endif
---
+++ wheel_code/wheel_code.ino
@@ -0,0 +1,244 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1325.h>
+#include "logo.h"
+#include "pins.h"
+#include "constants.h"
+#include "shift_register.h"
+
+#define max_page 2
+
+Adafruit_SSD1325 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+uint8_t disp_page; //display page
+
+//uint8_t rpm; //rpm
+//uint8_t odo; //miles
+uint8_t battery_temp; //Farenheit
+uint8_t t_pressure; //psi
+uint8_t dir; //0 = forward, 1 = neutral, 2 = reverse
+uint8_t spd;//speed in mph
+uint8_t LV_SOC;
+uint8_t HV_SOC;
+
+uint8_t IMD_FLT;
+uint8_t BMS_FLT;
+uint8_t SEVCON_FLT;
+uint8_t BRAKE_FLT;
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println('Display Startup');
+  //UI I/O
+  pinMode(SW1_L_IN, INPUT);
+  pinMode(SW1_R_IN, INPUT);
+  pinMode(TOGGLE_R_IN, INPUT);
+  pinMode(TOGGLE_L_IN, INPUT);
+  pinMode(LED_R, OUTPUT);
+  //Shift Register Interface
+  pinMode(SERIAL_1, OUTPUT);
+  pinMode(CLK_1, OUTPUT);
+  pinMode(CLR_1, OUTPUT);
+  pinMode(SERIAL_2, OUTPUT);
+  pinMode(CLK_2, OUTPUT);
+  pinMode(CLR_2, OUTPUT);
+  
+
+  attachInterrupt(digitalPinToInterrupt(SW1_L_IN), toggle_disp_up, RISING);
+  attachInterrupt(digitalPinToInterrupt(SW1_R_IN), toggle_disp_down, RISING);
+  display.begin();//initialize display
+
+  display.clearDisplay();
+  display.drawBitmap(0, 0,  LOGO, display.width(), display.height(), WHITE);
+  display.display();
+  delay(1000);
+  //test_leds(left);
+  //test_leds(right);
+  Wire.begin(8);
+  Wire.onReceive(receiveEvent); // register event
+
+  
+}
+
+void loop() {
+
+
+  //update variables to be displayed
+
+  //update LEDs
+ 
+  
+  switch (disp_page) {
+    case 0:
+      display.clearDisplay();
+      draw_page_0();
+      display.display();
+      break;
+    case 1:
+      display.clearDisplay();
+      draw_page_1();
+      display.display();
+      break;
+    default:
+      break;
+
+  }
+
+
+
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+
+  //rpm = Wire.read();
+  //odo = Wire.read();
+  battery_temp = Wire.read();
+  t_pressure = Wire.read();
+  dir = Wire.read();
+  spd = Wire.read();
+  LV_SOC = Wire.read();
+  HV_SOC = Wire.read();
+  
+  IMD_FLT = Wire.read();
+  BMS_FLT = Wire.read();
+  SEVCON_FLT = Wire.read();
+  BRAKE_FLT = Wire.read();
+  
+}
+
+
+
+
+void draw_dir(uint8_t dir) {
+
+  char dir_char = 'X';
+  uint16_t start_x = display.width() - dir_char_wid * 3;
+  uint16_t start_y = 0;
+  uint16_t offset = 4;
+  switch (dir) {
+    case 0:
+      dir_char = 'D';
+      break;
+    case 1:
+      dir_char = 'N';
+      break;
+    case 2:
+      dir_char = 'R';
+      break;
+    default:
+      break;
+  }
+
+  display.drawChar(start_x, start_y, dir_char, WHITE, BLACK, dir_char_scale);
+  display.drawFastVLine(start_x - offset, 0, dir_char_hgt * dir_char_scale, WHITE);
+  display.drawFastHLine(start_x - offset, start_y + dir_char_hgt * dir_char_scale, dir_char * dir_char_scale + offset, WHITE);
+}
+
+void draw_speed(uint8_t spd) {
+  uint8_t wid = display.width();
+  uint8_t hgt = display.height();
+  uint8_t offset = 5;
+  //draw numerals
+
+  display.setCursor(wid / 2 - spd_char_wid, (hgt / 2 - spd_char_hgt / 2) - offset);
+  display.setTextColor(WHITE, BLACK);
+  display.setTextSize(spd_char_scale);
+  display.setTextWrap(false);
+  display.print(spd);
+  //draw units
+
+  display.setCursor(wid / 2 - spd_char_wid + 2 * spd_char_wid + spd_unit_wid + 4, (hgt / 2 + spd_char_hgt / 2 - (spd_unit_hgt * 1.5)) - offset);
+  display.setTextColor(WHITE, BLACK);
+  display.setTextSize(spd_unit_scale);
+  display.setTextWrap(false);
+  display.print("mph");
+
+}
+
+void status_mesg(uint8_t IMD_STATE, uint8_t BMS_STATE, uint8_t SEVCON_STATE, uint8_t BRAKE_STATE) {
+
+  String  output = "";
+
+  if (IMD_STATE + BMS_STATE + SEVCON_STATE + BRAKE_STATE == 0) {
+    display.print("SYSTEM:OK");
+  }
+  else {
+    output = output + "ERR:" ;
+    if (IMD_STATE == 1) {
+      output = output + "IMD ";
+    }
+    if (BMS_STATE == 1) {
+      output = output + "BMS ";
+    }
+    if (SEVCON_STATE == 1) {
+      output = output + "SVCN ";
+    }
+    if (BRAKE_STATE == 1) {
+      output = output + "BRK ";
+    }
+    display.print(output);
+  }
+
+}
+
+void draw_stats(uint8_t temp, uint8_t pres, uint8_t IMD_STATE, uint8_t BMS_STATE, uint8_t SEVCON_STATE, uint8_t BRAKE_STATE) {
+
+
+  display.setCursor(0, 0);
+  display.setTextColor(WHITE, BLACK);
+  display.setTextSize(info_text_scale);
+  display.setTextWrap(false);
+  display.println("TMP:");
+  display.println(temp);
+  display.println("");
+  display.println("PSI:");
+  display.println(pres);
+  display.println("");
+  display.println("");
+  status_mesg(IMD_FLT, BMS_FLT, SEVCON_FLT, BRAKE_FLT);
+
+  display.drawFastVLine(info_text_wid * 5, 0, info_text_hgt * 4 + 10, WHITE);
+  display.drawFastHLine(0, info_text_hgt * 4 + 10, info_text_wid * 5, WHITE);
+  display.drawFastHLine(0, info_text_hgt * 2 + 3, info_text_wid * 5, WHITE);
+  display.drawFastHLine(0, display.height() - (info_text_hgt + 4), display.width(), WHITE);
+}
+
+
+void draw_page_0(void) {
+
+  //display.drawRect(0, 0, display.width(), display.height(), WHITE);
+  draw_dir(dir);
+  draw_speed(spd);
+  draw_stats(battery_temp, t_pressure, IMD_FLT, BMS_FLT, SEVCON_FLT, BRAKE_FLT);
+
+}
+
+void draw_page_1(void) {
+  display.drawCircle(display.width() / 2, display.height() / 2, 20, WHITE);
+}
+
+
+
+void toggle_disp_up() {
+
+  disp_page = disp_page + 1;
+
+  if (disp_page > max_page - 1) {
+    disp_page = 0;
+  }
+  Serial.println(disp_page);
+}
+
+void toggle_disp_down() {
+
+  disp_page = disp_page - 1;
+  if (disp_page < 0) {
+    disp_page = max_page - 1;
+  }
+  Serial.println(disp_page);
+}
+
